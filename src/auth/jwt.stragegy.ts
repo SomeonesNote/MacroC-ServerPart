c8048a83273b2c416a9950fa91b3e6cbab9bdb60
Yeongmin_Chae <codudals98@naver.com>
2023-09-21 16:22:45 +0900

    [Feat]생성 : 로그인 기능 위한 서버 구현 + 데이터 베이스 연결 #1

---
+++ src/auth/jwt.stragegy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: { username: string }) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOneBy({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,38 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { User } from './user.entity';
+import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(`Username : '${username}' already exists`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
