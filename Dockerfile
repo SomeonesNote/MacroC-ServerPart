ad603ff7f1f5ce67c1669e0735d4c4149bbd8ffe
Yeongmin_Chae <codudals98@naver.com>
2023-10-21 00:27:19 +0900

    [Feat]생성 : 도커 - NestJS with Postgres 도커라이징 후 도커 실행  #20

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,9 @@
-# build stage  
-FROM node:18-alpine AS build
+FROM node:18
+RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
-
-# prod stage
-FROM node:18-alpine
-WORKDIR /usr/src/app
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
-COPY --from=build /usr/src/app/dist ./dist
-COPY package*.json ./
-RUN npm install --only=production
-RUN rm package*.json
 EXPOSE 3000
 CMD ["node", "dist/main.js"]
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,9 +6,10 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbconfig.type,
   host: process.env.RDS_HOSTNAME || dbconfig.host,
   port: process.env.RDS_PORT || dbconfig.port,
+  url: process.env.RDS_URL || dbconfig.url,
   username: process.env.RDS_USERNAME || dbconfig.username,
   password: process.env.RDS_PASSWORD || dbconfig.password,
   database: process.env.RDS_DB_NAME || dbconfig.database,
-  entities: [__dirname + '/../**/*.entity.js'],
+  entities: [__dirname + '/../**/*.entity{.js,ts}'],
   synchronize: dbconfig.synchronize,
 };
