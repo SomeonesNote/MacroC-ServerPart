32371f79c5c50b67aefcb9212e7bf5579cef1688
Yeongmin_Chae <codudals98@naver.com>
2023-10-31 19:51:29 +0900

    [Feat]수정: 변수명(모듈 이름) 오타 수정 #21

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserFollowingController } from './user-following.controller';
 import { UserFollowingService } from './user-following.service';
+import { ArtistRepository } from 'src/artist/artist.repository';
+import { UserRepository } from 'src/auth/user.repository';
 
 @Module({
   imports: [],
   controllers: [UserFollowingController],
-  providers: [UserFollowingService],
+  providers: [UserFollowingService, ArtistRepository, UserRepository],
 })
-export class ArtistModule {}
+export class followModule {}
