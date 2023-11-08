d1ff058d07b50813a46a2e9abc76d5950134e6a9
Yeongmin_Chae <codudals98@naver.com>
2023-11-08 15:57:39 +0900

    [Feat]생성 : refreshToken 생성 기능 구현 + 필요한 라이브러리 설치 및 개발 사전 세팅 #24

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
   const serverConfig = config.get('server');
   const port = serverConfig.port;
