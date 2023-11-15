7f0e6c0a8f156304cb3c8649d669a771a99f3357
Yeongmin_Chae <codudals98@naver.com>
2023-11-16 00:04:59 +0900

    [Feat]수정 : refreshToken을 json객체에 담아, client에서 쉽게 해독할 수 있도록 코드 수정

--- src/auth/apple-auth.controller.ts
+++ src/auth/apple-auth.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AppleAuthService } from './apple-auth.service';
 
 @Controller('apple-auth')
 export class AppleAuthController {
   constructor(private appleAuthService: AppleAuthService) {}
 
-  @Get('/refreshToken')
-  async getRefreshToken(@Query('code') code: string): Promise<string> {
+  @Post('/refreshToken')
+  async getRefreshToken(
+    @Body('code') code: string,
+    @Res() res,
+  ): Promise<{ refreshToken: string }> {
     const refreshToken = await this.appleAuthService.getRefreshToken(code);
-    return refreshToken;
+    return res.json({ refreshToken });
   }
 
   @Get('/revoke')
