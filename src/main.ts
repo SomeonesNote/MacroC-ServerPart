aebbaa769aa8e22cdd41e265ef56c8f8d26eec97
Yeongmin_Chae <codudals98@naver.com>
2023-11-17 16:21:50 +0900

    [Feat]생성 : 배포 페이지, html 요소 추가 기능 개발 세팅 #40

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const serverConfig = config.get('server');
   const port = serverConfig.port;
 
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   console.log(`port ${port}`);
-  await app.listen(port, '0.0.0.0'); // fly.io deployment
-  // await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
   Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
